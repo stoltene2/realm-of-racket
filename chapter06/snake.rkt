70a5a81409e8d4782bf12ffcfd153394fd0db90c
Eric Stolten <stoltene2@gmail.com>
2018-01-23 08:28:38 -0500

    First working impl of snake

--- chapter06/snake.rkt
+++ chapter06/snake.rkt
@@ -7,13 +7,40 @@
 
 (define TICK-RATE 1)
 (define SIZE 10)
+(define ENDGAME-TEXT-SIZE (* 10 SIZE))
 (define EXPIRATION-TIME 10)
+(define EMPTY-SCENE (empty-scene (sqr SIZE) (sqr SIZE)))
+(define SEG-SIZE SIZE)
+(define SEG-IMG (square SIZE "solid" "blue"))
+(define GOO-IMG (square SIZE "solid" "green"))
+(define HEAD-UP-IMG (square SIZE "solid" "red"))
+(define HEAD-LEFT-IMG (square SIZE "solid" "red"))
+(define HEAD-RIGHT-IMG (square SIZE "solid" "red"))
+(define HEAD-DOWN-IMG (square SIZE "solid" "red"))
+
+;;------------------------------------------------------------------------------
+#|
+Ideas
+
+- Make a background tiling
+- Make obstacles
+- Make the goo change color as it ages
+- Make random times for goo expiration
+- Keep track of max length and score
+- Make snake grow by more than a segment if needed
+
+- Allow a configuration function to be passed into start so that we can control how things
+  are created.
+  - Different strategy for creating goos
+|#
 
 ;;------------------------------------------------------------------------------
 
 (struct pit [snake goos] #:transparent)
+(define-struct-updaters pit)
 
 (struct snake [dir segs] #:transparent)
+(define-struct-updaters snake)
 
 (struct posn [x y] #:transparent)
 
@@ -22,64 +49,75 @@
 
 ;;------------------------------------------------------------------------------
 
-(define (fresh-goo)
-  (goo (posn
-        (add1 (random (sub1 SIZE)))
-        (add1 (random (sub1 SIZE))))
-       EXPIRATION-TIME))
 
-
-(define (render-pit w)
-  (square 10 'solid 'blue))
 
 
 (define (next-pit w)
-  (define snake (pit-snake w))
+  (define sn (pit-snake w))
   (define goos (pit-goos w))
-  (define goo-to-eat (can-eat snake goos))
+  (define goo-to-eat (can-eat sn goos))
   (if goo-to-eat
-      (pit (grow snake) (age-goo (eat goos goo-to-eat)))
-      (pit (slither snake) (age-goo goos))))
+      (pit (grow sn) (age-goo (eat goos goo-to-eat)))
+      (pit (slither sn) (age-goo goos))))
 
 
-;; TODO: Implement
-(define (direct-snake w key)
-  (cond [else w]))
+;; Keyboard navigation
+(define (direct-snake w ke)
+  (define (dir? x)
+    (or [key=? x "up"]
+        [key=? x "down"]
+        [key=? x "left"]
+        [key=? x "right"]))
+
+  (cond [(dir? ke) (world-change-dir w ke)]
+        [else w]))
 
 
-;; TODO: Implement
+(define (world-change-dir w d)
+  (define sn (pit-snake w))
+  (define turned-on-self? (opposite-dir? (snake-dir sn) d))
+  (define has-body? (cons? (rest (snake-segs sn))))
+
+  (cond [(and turned-on-self? has-body?) (stop-with w)]
+        [else (pit (snake-change-dir sn d) (pit-goos w))]))
+
+;; key-dir must be a direction
+(define (opposite-dir? sn-dir key-dir)
+  (define (opposite dir)
+    (cond [(string=? dir "up") "down"]
+          [(string=? dir "down") "up"]
+          [(string=? dir "left") "right"]
+          [(string=? dir "right") "left"]))
+  (string=? sn-dir (opposite key-dir)))
+
+
 (define (dead? w)
-  #f)
+  (define sn (pit-snake w))
+  (or (self-colliding? sn) (wall-colliding? sn)))
+
+(define (self-colliding? sn)
+  (cons? (member (snake-head sn) (snake-body sn))))
+
+(define (wall-colliding? sn)
+  (define x (posn-x (snake-head sn)))
+  (define y (posn-y (snake-head sn)))
+  (or (= 0 x) (= x SIZE)
+      (= 0 y) (= y SIZE)))
 
 
-(define (can-eat snake goos)
+(define (can-eat sn goos)
   (cond [(empty? goos) #f]
-        [else (if (close? (snake-head snake) (first goos))
+        [else (if (close? (snake-head sn) (first goos))
                   (first goos)
-                  (can-eat snake (rest goos)))]))
+                  (can-eat sn (rest goos)))]))
 
 
-(define (age-goo goos)
-  (define (decay g)
-    (goo-expire-update g sub1))
 
-  (define aged (map decay goos))
-
-  ;;TODO: Replace rotten goo with a new random goo
-  (filter (lambda (x) (> (goo-expire x) 0))
-          aged))
-
-
-(define (grow snake)
-  (snake (snake-dir snake))
-  (cons (next-head snake) (snake-segs snake)))
-
-
-(define (eat goos goo-to-eat)
-  (cons (fresh-goo) (remove goo-to-eat goos)))
 
 ;; TODO: Implement
-(define (render-end w) empty)
+(define (render-end w)
+  (overlay (text "Game Over" ENDGAME-TEXT-SIZE "red")
+           (render-pit w)))
 
 ;;- Snake Query Functions ------------------------------------------------------
 
@@ -112,11 +150,41 @@
 
 ;; TODO: Review this
 (define (snake-change-dir sn d)
-  (snake d (snake-segs sn)))
+  (snake-dir-set sn d))
 
 (define (slither sn)
   (snake (snake-dir sn)
-         (cons (next-head snake) (all-but-last (snake-segs sn)))))
+         (cons (next-head sn) (all-but-last (snake-segs sn)))))
+
+
+;; NOTE: I had some problems with naming and variable shadowing. Error wasn't easy to debug.
+;; I called an argument snake instead of sn
+(define (grow sn)
+  (snake (snake-dir sn) (cons (next-head sn) (snake-segs sn))))
+
+
+;;- Goo ------------------------------------------------------------------------
+
+(define (eat goos goo-to-eat)
+  (cons (fresh-goo) (remove goo-to-eat goos)))
+
+
+(define (age-goo goos)
+  (define (decay g)
+    (goo-expire-update g sub1))
+
+  (define aged (map decay goos))
+
+  ;;TODO: Replace rotten goo with a new random goo
+  (filter (lambda (x) (> (goo-expire x) 0))
+          aged))
+
+
+(define (fresh-goo)
+  (goo (posn
+        (add1 (random (sub1 SIZE)))
+        (add1 (random (sub1 SIZE))))
+       EXPIRATION-TIME))
 
 
 ;;- Helper functions -----------------------------------------------------------
@@ -135,6 +203,39 @@
   (cond [(empty? lst) lst]
         [else (take lst (sub1 len))]))
 
+;;- Rendering ------------------------------------------------------------------
+
+(define (render-pit w)
+  (snake+scene (pit-snake w)
+               (goo-list+scene (pit-goos w) EMPTY-SCENE)))
+
+(define (snake+scene sn scene)
+  (define snake-body-scene
+    (img-list+scene (snake-body sn) SEG-IMG scene))
+  (define dir (snake-dir sn))
+  (img+scene (snake-head sn)
+             (cond [(string=? "up" dir) HEAD-UP-IMG]
+                   [(string=? "down" dir) HEAD-DOWN-IMG]
+                   [(string=? "left" dir) HEAD-LEFT-IMG]
+                   [(string=? "right" dir) HEAD-RIGHT-IMG])
+             snake-body-scene))
+
+
+(define (img-list+scene posns img scene)
+  (cond [(empty? posns) scene]
+        [else (img+scene
+               (first posns)
+               img
+               (img-list+scene (rest posns) img scene))]))
+
+(define (img+scene posn img scene)
+  (place-image img
+               (* (posn-x posn) SEG-SIZE)
+               (* (posn-y posn) SEG-SIZE)
+               scene))
+
+(define (goo-list+scene goos scene)
+  (img-list+scene (map goo-loc goos) GOO-IMG scene))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Big Bang
 
@@ -206,3 +307,21 @@
               (list (goo (posn 1 1) 1))
 
               "Remove expired goo from the list")
+
+
+;;- Changing directions --------------------------------------------------------
+
+#|
+> (define snake-going-left (snake "left" (list (posn 2 18) (posn 2 19))))
+> (define plain-world (pit snake-going-left empty))
+> (world-change-dir plain-world "right")
+(stop-the-world (pit (snake "left" (list (posn 2 18) (posn 2 19))) '()))
+
+
+(define snake-going-left (snake "left" (list (posn 2 18))))
+> (define plain-world (pit snake-going-left empty))
+> (world-change-dir plain-world "right")
+(pit (snake "right" (list (posn 2 18))) '())
+>
+
+|#
